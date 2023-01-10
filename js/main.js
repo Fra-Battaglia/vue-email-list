ee232cf7477164300e30eb122e57737fffc3c7d7
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-01-10 15:48:54 +0100

    exercise done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-createApp ({
+const { createApp } = Vue
+
+createApp({
 	data() {
 		return {
 			array_mail: []
 		}
 	},
 
-	methods:{
-
+	methods: {
+		push_mail() {
+			for (let i = 0; i < 10; i++) {
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+					.then((result) => {
+						this.mail = result.data.response,
+							this.array_mail.push(this.mail)
+					})
+			}
+		}
 	}
-	
+
 }).mount('#app')
